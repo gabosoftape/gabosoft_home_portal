242b34fbb40d23345509160e81b2069ff3996814
Gab <gabosoft>
2020-07-13 14:44:11 -0500

    test jquery into fadeIn and fadeOut

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -1,24 +1,9 @@
-// init controller
-var controller = new ScrollMagic.Controller();
 
-// create a scene
-new ScrollMagic.Scene({
-    duration: 100, // the scene should last for a scroll distance of 100px
-    offset: 1050 // start this scene after scrolling for 50px
-})
-    .setPin('#productos') // pins the element for the the scene's duration
-    .addTo(controller); // assign the scene to the controller
-
-new ScrollMagic.Scene({
-    duration: 100, // the scene should last for a scroll distance of 100px
-    offset: 1150 // start this scene after scrolling for 50px
-})
-    .setPin('#soluciones') // pins the element for the the scene's duration
-    .addTo(controller); // assign
-
-new ScrollMagic.Scene({
-    duration: 100, // the scene should last for a scroll distance of 100px
-    offset: 1250 // start this scene after scrolling for 50px
-})
-    .setPin('#productos') // pins the element for the the scene's duration
-    .addTo(controller); // assign
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 800) {
+    $('#productos').fadeIn();
+  } else if (y > 1000) {
+    $('#productos').fadeOut();
+  }
+});
