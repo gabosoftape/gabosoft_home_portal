8f84237931017fc08646d4741fade6116445dd44
Gab <gabosoft>
2020-07-13 16:48:45 -0500

    update sections of the example

--- static/src/js/custom.js
+++ static/src/js/custom.js
@@ -487,15 +487,24 @@ $(function () { // wait for document ready
 
     // define movement of panels
     var wipeAnimation = new TimelineMax()
-        .fromTo("#productos", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
-        .fromTo("#soluciones",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
-        .fromTo("#ultima", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
+        // animate to second panel
+        .to("#slideContainer", 0.5, {z: -150})		// move back in 3D space
+        .to("#slideContainer", 1,   {x: "-25%"})	// move in to first panel
+        .to("#slideContainer", 0.5, {z: 0})				// move back to origin in 3D space
+        // animate to third panel
+        .to("#slideContainer", 0.5, {z: -150, delay: 1})
+        .to("#slideContainer", 1,   {x: "-50%"})
+        .to("#slideContainer", 0.5, {z: 0})
+        // animate to forth panel
+        .to("#slideContainer", 0.5, {z: -150, delay: 1})
+        .to("#slideContainer", 1,   {x: "-75%"})
+        .to("#slideContainer", 0.5, {z: 0});
 
     // create scene to pin and link animation
     new ScrollMagic.Scene({
             triggerElement: "#pinContainer",
             triggerHook: "onLeave",
-            duration: "300%"
+            duration: "500%"
         })
         .setPin("#pinContainer")
         .setTween(wipeAnimation)
