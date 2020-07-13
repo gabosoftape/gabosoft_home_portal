bbbf83bfe914712a7a55d6f25ede440965becb3a
Gab <gabosoft>
2020-07-13 14:48:13 -0500

    update custom script and remove callback of the appjs

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -1,9 +1,1 @@
 
-$(document).scroll(function() {
-  var y = $(this).scrollTop();
-  if (y > 800) {
-    $('#productos').fadeIn();
-  } else if (y > 1000) {
-    $('#productos').fadeOut();
-  }
-});
--- static/src/js/custom.js
+++ static/src/js/custom.js
@@ -469,6 +469,7 @@ function simpleMap(latitude, longitude, markerImage, mapStyle, mapElement, marke
 
 
 $(function () {
+    var y = $(this).scrollTop();
     $(window).scroll(function () {
         if ($(this).scrollTop() > 500) {
             $('.navbar .navbar-brand img').attr('src','/gabosoft_home_portal/static/src/img/logo.png');
@@ -478,6 +479,10 @@ $(function () {
             $('.navbar .navbar-brand img').attr('src','/gabosoft_home_portal/static/src/svg/motion-logo-negativo-tagline.svg');
             $('.navbar .navbar-brand img').addClass('svg_l');
         }
+        if (y > 800) {
+            $('#productos').fadeIn();
+        }
     })
 });
+
 
