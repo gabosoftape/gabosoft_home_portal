933e335209f6bcfbaf425772179fdef51b3b51d4
Gab <gabosoft>
2020-07-13 12:45:27 -0500

    test appjs

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -6,5 +6,19 @@ new ScrollMagic.Scene({
     duration: 100, // the scene should last for a scroll distance of 100px
     offset: 50 // start this scene after scrolling for 50px
 })
-    .setPin('#my-sticky-element') // pins the element for the the scene's duration
+    .setPin('#productos') // pins the element for the the scene's duration
     .addTo(controller); // assign the scene to the controller
+
+new ScrollMagic.Scene({
+    duration: 100, // the scene should last for a scroll distance of 100px
+    offset: 150 // start this scene after scrolling for 50px
+})
+    .setPin('#soluciones') // pins the element for the the scene's duration
+    .addTo(controller); // assign
+
+new ScrollMagic.Scene({
+    duration: 100, // the scene should last for a scroll distance of 100px
+    offset: 250 // start this scene after scrolling for 50px
+})
+    .setPin('#productos') // pins the element for the the scene's duration
+    .addTo(controller); // assign
