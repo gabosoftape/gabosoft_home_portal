7163675c69fde113fe8f39db470864a2bfa57f7a
Gab <gabosoft>
2020-07-29 15:33:41 -0500

    update template

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -1,7 +1,6 @@
 var tlFirstScroll = new TimelineMax()
 
 tlFirstScroll
-.set('#productos', {scale: 4, transformOrigin: "center top"})
 .to('#productos', 3, {scale:2.2, y:"-50%"})
 .to('#productos', 3 ,{scale: 1, y:"0%"})
 
