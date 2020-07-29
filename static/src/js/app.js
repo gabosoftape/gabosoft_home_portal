c788188f72ee32285b03aeeb8f393ccb88bf6e07
Gab <gabosoft>
2020-07-29 15:32:23 -0500

    update template

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -1,9 +1,9 @@
 var tlFirstScroll = new TimelineMax()
 
 tlFirstScroll
-.set('productos', {scale: 4, transformOrigin: "center top"})
-.to('productos', 3, {scale:2.2, y:"-50%"})
-.to('productos', 3 ,{scale: 1, y:"0%"})
+.set('#productos', {scale: 4, transformOrigin: "center top"})
+.to('#productos', 3, {scale:2.2, y:"-50%"})
+.to('#productos', 3 ,{scale: 1, y:"0%"})
 
 var controller = new ScrollMagic.Controller();
 
