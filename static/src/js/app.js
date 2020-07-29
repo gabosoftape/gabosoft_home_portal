03bded3b9e5fa0d396795c337e36aee548fceb80
Gab <gabosoft>
2020-07-29 12:27:19 -0500

    testing animation js

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -1,3 +1,10 @@
 var tlFirstScroll = new TimelineMax()
+
 tlFirstScroll
-.set()
+.set('.product-div', {scale: 4, transformOrigin: "center top"})
+.to('.product-div', 3, {scale:2.2, y:"-50%"})
+.to('.product-div', 3 ,{scale: 1, y:"0%"})
+
+var controller = new ScrollMagic.Controller();
+
+//Escena1
