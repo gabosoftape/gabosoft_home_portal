72e68d501c6b5ed40d216f906e0dc6a6c89d3232
Gab <gabosoft>
2020-08-03 12:06:56 -0500

    test update

--- static/src/js/custom.js
+++ static/src/js/custom.js
@@ -150,7 +150,7 @@ $(document).ready(function($) {
             //height: (1- $(this).attr("data-wave-height")) * $(window).height(),
             height: $(this).attr("data-wave-height"),
             bones: $(this).attr("data-wave-bones"),
-            amplitude: .02 * $(window).height(),
+            amplitude: .08 * $(window).height(),
             color: $(this).attr("data-wave-color"),
             speed: .15
         });
